27582a5b2e6dbc2dd98cbf22cf10fd1464a868bb
Abdul Khan <akhan17@outlook.com>
2020-09-14 16:44:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solutions in this file
 function writeCards(arr, title){
   for(let i = 0; i <= arr.length; i++){
-  `Thank you, ${arr[i]}, for the wonderful ${title} gift!`
+    return `Thank you, ${arr[i]}, for the wonderful ${title} gift!`
   }
 }
