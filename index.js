0c34eea742253942a21752385b453baba769e65a
Abdul Khan <akhan17@outlook.com>
2020-09-14 16:55:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(arr, title){
+  for(let i = 0; i < arr.length; i++){
+    return `Thank you, ${arr[i]}, for the wonderful ${title} gift!`
+  }
+}
+
+
+function countDown(num){
+  let i = num;
+  while (i >= 0){
+    console.log(i)
+    i--
+  }
+}
